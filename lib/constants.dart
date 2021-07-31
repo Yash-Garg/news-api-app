4132d9f6349b5f5943074cb4a7df7eeb905afd0e
Yash-Garg <yash.garg@outlook.in>
2021-07-31 15:59:47 +0530

    chore: get 50 results per page
    Signed-off-by: Yash-Garg <yash.garg@outlook.in>

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,7 @@
 class Constants {
   static const API_KEY = '';
   static const BASE_URL =
-      'https://newsapi.org/v2/top-headlines?country=in&apiKey=$API_KEY';
+      'https://newsapi.org/v2/top-headlines?country=in&apiKey=$API_KEY&pagesize=50';
   static const PLACEHOLDER =
       'https://raw.githubusercontent.com/Yash-Garg/news-api-app/develop/assets/placeholder.jpg';
 }
